701e53b8b2923009e12880ffae6aaae2354965f9
tranhuyviet <44466617+tranhuyviet@users.noreply.github.com>
2021-02-05 12:24:08 +0200

    implemented Gallery Section to show images lightbox

--- src/components/GallerySection/GallerySection.js
+++ src/components/GallerySection/GallerySection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import Title from '../Title/Title';
 import { useStyles } from './styles';
@@ -13,41 +13,46 @@ import gallery6 from '../../images/gallery6.webp';
 import gallery7 from '../../images/gallery7.webp';
 import gallery8 from '../../images/gallery8.webp';
 
+import Lightbox from 'react-awesome-lightbox';
+import 'react-awesome-lightbox/build/style.css';
+
 const GallerySection = () => {
     const classes = useStyles();
+    const [isOpen, setIsOpen] = useState(false);
+    const [imgIndex, setImgIndex] = useState(0);
+    const images = [
+        { url: gallery1 },
+        { url: gallery2 },
+        { url: gallery3 },
+        { url: gallery4 },
+        { url: gallery5 },
+        { url: gallery6 },
+        { url: gallery7 },
+        { url: gallery8 },
+    ];
+
+    const handleImageClick = (index) => {
+        setImgIndex(index);
+        setIsOpen(true);
+    };
+
+    const hadleClose = () => {
+        setIsOpen(false);
+    };
+
     return (
         <section className={classes.gallerySection}>
             <Title title="Gallery" />
             <Grid container className={classes.gridContainer}>
                 <Grid item sm={12} container>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery1} alt="galerry" className={classes.image} />
-                    </Grid>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery2} alt="galerry" className={classes.image} />
-                    </Grid>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery3} alt="galerry" className={classes.image} />
-                    </Grid>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery4} alt="galerry" className={classes.image} />
-                    </Grid>
-                </Grid>
-                <Grid item sm={12} container>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery5} alt="galerry" className={classes.image} />
-                    </Grid>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery6} alt="galerry" className={classes.image} />
-                    </Grid>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery7} alt="galerry" className={classes.image} />
-                    </Grid>
-                    <Grid item sm={3} className={classes.gridItem}>
-                        <img src={gallery8} alt="galerry" className={classes.image} />
-                    </Grid>
+                    {images.map((image, index) => (
+                        <Grid item sm={3} className={classes.gridItem} key={index}>
+                            <img src={image.url} alt="galerry" className={classes.image} onClick={() => handleImageClick(index)} />
+                        </Grid>
+                    ))}
                 </Grid>
             </Grid>
+            {isOpen && <Lightbox images={images} onClose={hadleClose} startIndex={imgIndex} />}
         </section>
     );
 };
